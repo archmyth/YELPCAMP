6bc9b64ea237faaa70c364b1f43178eb838b69c4
Xiang zgit config --global user.name Xiang <xiang819zheng@yahoo.com>
2023-05-09 23:53:52 -0400

    14

--- routes/users.js
+++ routes/users.js
@@ -4,32 +4,15 @@ const { storeReturnTo } = require('../middleware');
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
-
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
-
-router.post('/login',
+router.route('/login')
+.get(users.renderLogin)
+.post(
     // use the storeReturnTo middleware to save the returnTo value from session to res.locals
     storeReturnTo,
     // passport.authenticate logs the user in and clears req.session
@@ -39,23 +22,11 @@ router.post('/login',
         req.flash('success', 'Welcome back!');
         const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
         res.redirect(redirectUrl);
-    });
+    })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
+.post( passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-});
+
+router.get('/logout', users.logout);
 
 module.exports = router;
