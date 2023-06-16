2ac6f2669e6d232cf2a184f86992c4257dddd3f5
Xiang zgit config --global user.name Xiang <xiang819zheng@yahoo.com>
2023-06-16 15:01:04 -0400

    add atlas

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
+const ejsMate=require('ejs-mate');
+
 const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
@@ -18,19 +19,21 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
-const reviewRoutes = require('./routes/reviews');
-
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+const reviewRoutes=require('./routes/reviews');
+const MongoStore=require("connect-mongo");
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+// const dbUrl=process.env.DB_URl;
+// 'mongodb://127.0.0.1:27017/yelp-camp'
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-
 });
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
-
 
 const app = express();
 
@@ -44,11 +47,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
-    
+    store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -59,11 +73,13 @@ const sessionConfig = {
     }
 }
 
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet());
+
+
 const scriptSrcUrls = [
-    "https://cdn.jsdelivr.net",
+    "https://stackpath.bootstrapcdn.com",
     "https://api.tiles.mapbox.com",
     "https://api.mapbox.com",
     "https://kit.fontawesome.com",
@@ -72,7 +88,7 @@ const scriptSrcUrls = [
 ];
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com",
-    "https://cdn.jsdelivr.net",
+    "https://stackpath.bootstrapcdn.com",
     "https://api.mapbox.com",
     "https://api.tiles.mapbox.com",
     "https://fonts.googleapis.com",
@@ -98,7 +114,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
@@ -107,14 +123,12 @@ app.use(
 );
 
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
@@ -144,6 +158,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving at http://localhost:${port}`)
 })
