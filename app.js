c116bd338c6795f18c3bc5bcf4df7a876530d498
Xiang zgit config --global user.name Xiang <xiang819zheng@yahoo.com>
2023-06-16 18:02:07 -0400

    app1

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes=require('./routes/reviews');
 const MongoStore=require("connect-mongo");
-const dbUrl = process.env.DB_URL;
-// const dbUrl=process.env.DB_URl;
+const dbUrl = process.env.DB_URL ||  'mongodb://127.0.0.1:27017/yelp-camp';
+
 // 'mongodb://127.0.0.1:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
